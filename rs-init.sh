a4559e4a20677212272c749e88f335619a42bbfa
AdsonCicilioti <eu@adsonagencia.com>
2022-08-24 14:09:29 -0300

    better name refs

--- rs-init.sh
+++ rs-init.sh
@@ -6,22 +6,21 @@ var config = {
 	"version": 1,
 	"members" : [
 		{
-			"_id" : 1,
+			"_id" : 0,
 			"host" : "habafdb1:27017",
 			"priority": 3
 		},
 		{
-			"_id" : 0,
+			"_id" : 1,
 			"host" : "habafdb2:27017",
 			"priority": 2
 		},
 		{
-			"_id" : 3,
+			"_id" : 2,
 			"host" : "habafdb3:27017",
 			"priority": 1
 		}
 	]
 };
 rs.initiate(config, { force: true });
-rs.status();
 EOF
--- run-db.sh
+++ run-db.sh
@@ -4,4 +4,4 @@ docker-compose up -d
 
 sleep 3
 
-docker exec habilitados_db_1 /scripts/rs-init.sh
+docker exec habafdb1 /scripts/rs-init.sh
